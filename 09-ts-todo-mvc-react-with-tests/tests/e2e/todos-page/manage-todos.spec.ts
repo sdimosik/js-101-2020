3acac51dc646c4951db3a9b7dc5531719e4b6476
Shabinsky Dmitry <sdimosikvip@gmail.com>
2021-03-30 00:13:11 +0300

    hw2

--- 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
+++ 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
@@ -1,6 +1,6 @@
 import { todosEndpoint, getUserEndpoint, pageUrl } from '../../../src/consts/urls';
 
-function authorize() {
+export function authorize() {
   cy.intercept(
     'GET',
     getUserEndpoint,
@@ -18,41 +18,65 @@ function authorize() {
     .wait('@initialTodos');
 }
 
+export function createTodo() {
+  const itemText = Math.random().toString() + '_' + Date.now();
+
+  cy.get('[data-test-id=create-new-todo-form]').should('be.visible');
+  cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
+  cy.get('[data-test-id=create-new-todo-form]').submit();
+  return itemText;
+}
+
+beforeEach('', () => {
+  cy.fixture('todo-item.response.json').then((todoItemResponse) => {
+    cy.intercept(
+        'POST',
+        todosEndpoint,
+        req => {
+          const {body} = req;
+          req.reply({
+            statusCode: 200,
+            body: JSON.stringify({
+              ...todoItemResponse,
+              text: body.text
+            })
+          });
+        }
+    ).as('createTodo');
+  });
+})
+
 describe('Manage Todos', () => {
   context('Creation', () => {
     it('Create todo', () => {
-      cy.fixture('todo-item.response.json').then((todoItemResponse)  => {
-        cy.intercept(
-          'POST',
-          todosEndpoint,
-          req => {
-            const { body } = req;
-            req.reply({
-              statusCode: 200,
-              body: JSON.stringify({
-                ...todoItemResponse,
-                text: body.text
-              })
-            });
-          }
-        ).as('createTodo');
-      });
-
-      const itemText = Math.random().toString() + '_' + Date.now();
 
       authorize();
-      cy.get('[data-test-id=create-new-todo-form]').should('be.visible');
-      cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
-      cy.get('[data-test-id=create-new-todo-form]').submit();
+
+      const itemText = createTodo();
 
       cy.wait('@createTodo');
 
       cy.get('[data-test-id=todos-list]').should('be.visible');
-
       cy.get('[data-test-id=todo-item]').should('be.visible');
       cy.get('[data-test-id=todo-item__checked-checkbox]').should('not.be.checked');
       cy.get('[data-test-id=todo-item__text-input]').should('have.value', itemText);
       cy.get('[data-test-id=todo-item__remove-action]').should('be.visible');
     });
   });
+  context('Deletion', () => {
+    it('Delete todo', () => {
+      authorize();
+
+      createTodo();
+
+      cy.wait('@createTodo');
+
+      cy.get('[data-test-id=todos-list]').should('be.visible');
+      cy.get('[data-test-id=todo-item]').should('be.visible');
+      cy.get('[data-test-id=todo-item__remove-action]').should('be.visible');
+      cy.get('[data-test-id=todo-item__remove-action]').click({ force: true });
+
+      cy.get('[data-test-id=todo-item]').should('not.exist');
+    })
+  })
 });
